b8a2cc818187a59515b2cff87afd99d84731013e
grigoni <giovanni.rigoni@smartmobile.it>
2022-06-08 18:09:55 +0200

    fix(main): update business logic - good version

--- bin/lang_converter.dart
+++ bin/lang_converter.dart
@@ -4,18 +4,20 @@ import 'dart:io';
 import 'localization_dictionary_converter.dart';
 
 void main(List<String> args) {
-  final lang = "fr";
-  final sourceFilename = "locale-$lang";
+  final languages = <String>["it", "en", "fr"];
+  for (var lang in languages) {
+    final sourceFilename = "locale-$lang";
 
-  Future<Map<String, dynamic>> fileFuture = _readJsonFile(sourceFilename);
-  fileFuture.then((value) {
-    final localizator = LocalizationDictionaryConverter(source: value, sourceFilename: sourceFilename, lang: lang);
-    localizator.converter();
-  });
+    Future<Map<String, dynamic>> fileFuture = _readJsonFile(sourceFilename);
+    fileFuture.then((value) {
+      final localizator = LocalizationDictionaryConverter(source: value, sourceFilename: sourceFilename, lang: lang);
+      localizator.converter();
+    });
+  }
 }
 
-Future<Map<String, dynamic>> _readJsonFile(String filenameServer) async {
-  final file = File('bin/assets/$filenameServer.json');
+Future<Map<String, dynamic>> _readJsonFile(String sourceFilename) async {
+  final file = File('bin/assets/input/$sourceFilename.json');
   final content = await file.readAsString();
   final instance = jsonDecode(content);
   return instance;
--- bin/localization_dictionary_converter.dart
+++ bin/localization_dictionary_converter.dart
@@ -1,6 +1,9 @@
+import 'dart:collection';
 import 'dart:io';
 
-import 'assets/it.dart';
+import 'assets/template/it.dart';
+import 'assets/template/en.dart';
+import 'assets/template/fr.dart';
 
 class LocalizationDictionaryConverter {
   final Map<String, dynamic> source;
@@ -10,13 +13,28 @@ class LocalizationDictionaryConverter {
   LocalizationDictionaryConverter({required this.source, required this.sourceFilename, required this.lang});
 
   void converter() {
-    final tmp = "converted-to-flat";
-    Map<String, String> result = {};
+    Map<String, String> template = {};
+    switch (lang) {
+      case "it":
+        template = kimoLocaleIT;
+        break;
+      case "en":
+        template = kimoLocaleEN;
+        break;
+      case "fr":
+        template = kimoLocaleFR;
+        break;
+
+      default:
+    }
+
+    final result = SplayTreeMap<String, String>((a, b) => a.toLowerCase().compareTo(b.toLowerCase()));
     _fromServerToClientMapping(source, result, "");
-    _createNewFile(result, tmp, lang, "dart");
-    addMissingKeysFromTemplate(kimoLocaleIT, result);
-    _createNewFile(result, lang.toLowerCase(), lang.toUpperCase(), "json");
-    _removeFile(tmp);
+    _createNewFile(result, "tmp_${lang.toLowerCase()}", "dart");
+    _addMissingKeysFromTemplate(template, result);
+    _createNewFile(result, lang, "dart");
+    _createNewFile(result, lang, "json");
+    _removeFile("tmp_${lang.toLowerCase()}", "dart");
   }
 
   void _fromServerToClientMapping(Map<String, dynamic> source, Map<String, String> result, String parentKey) {
@@ -24,7 +42,8 @@ class LocalizationDictionaryConverter {
       (key, value) {
         if (value is String) {
           final fullKey = parentKey + key;
-          result.putIfAbsent(fullKey, () => value.replaceAll("\n", "\\n"));
+          result.putIfAbsent(
+              fullKey, () => value.replaceAll("\n", "\\n").replaceAll("\\'", "'").replaceAll("""\"""", ""));
         } else {
           final nextParentKey = parentKey + key + ".";
           _fromServerToClientMapping(value, result, nextParentKey);
@@ -33,28 +52,36 @@ class LocalizationDictionaryConverter {
     );
   }
 
-  void addMissingKeysFromTemplate(Map<String, String> templateMap, Map<String, String> importedMap) {
+  void _addMissingKeysFromTemplate(Map<String, String> templateMap, Map<String, String> importedMap) {
     for (final templateEntry in templateMap.entries) {
-      importedMap.putIfAbsent(templateEntry.key, () => templateEntry.value.replaceAll("\n", "\\n"));
+      importedMap.putIfAbsent(templateEntry.key,
+          () => templateEntry.value.replaceAll("\n", "\\n").replaceAll("\\'", "'").replaceAll("""\"""", ""));
     }
   }
 
-  void _createNewFile(Map<String, String> map, String filename, String lang, String extension) async {
+  void _createNewFile(Map<String, String> map, String lang, String extension) async {
     StringBuffer sb = StringBuffer();
-    // sb.writeln("Map<String, String> kimoLocale$lang = {");
+    if (extension.contains("dart")) {
+      sb.writeln("Map<String, String> kimoLocale${lang.toUpperCase()} = ");
+    }
     sb.writeln("{");
     map.forEach((key, value) {
       sb.writeln("   \"$key\" : \"$value\",");
     });
 
     String finalValue = sb.toString();
-    finalValue = finalValue.replaceRange(finalValue.length - 2, finalValue.length - 1, "\n}");
-    final file = File('bin/assets/$filename.$extension');
+    if (extension.contains("dart")) {
+      finalValue = finalValue.replaceRange(finalValue.length - 2, finalValue.length - 1, "\n};");
+    } else {
+      finalValue = finalValue.replaceRange(finalValue.length - 2, finalValue.length - 1, "\n}");
+    }
+
+    final file = File('bin/assets/output/${lang.toLowerCase()}.$extension');
     await file.writeAsString(finalValue);
   }
 
-  void _removeFile(String filenameToRemove) {
-    final file = File('bin/assets/$filenameToRemove.dart');
+  void _removeFile(String lang, String extension) {
+    final file = File('bin/assets/output/${lang.toLowerCase()}.$extension');
     file.delete();
   }
 }
